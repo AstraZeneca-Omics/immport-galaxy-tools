c7471c7bda8f227bc87f9d73f941c40d5ac04f54
emily101-gif <52698020+emily101-gif@users.noreply.github.com>
2020-06-17 19:05:57 +0100

    Add correct R script

--- flowtools/fcs_summary/FCSstats_txt.py
+++ flowtools/fcs_summary/FCSstats_txt.py
@@ -1,43 +1,58 @@
-#!/usr/bin/env python
-
+#!/usr/bin/Rscript --vanilla
+# FCS Summary Statistic Module for Galaxy
+# FlowCore
 ######################################################################
 #                  Copyright (c) 2016 Northrop Grumman.
 #                          All rights reserved.
 ######################################################################
+#
+# Version 1
+# Cristel Thomas
+#
+#
 
-from __future__ import print_function
-import pandas as pd
-from argparse import ArgumentParser
+library(flowCore)
+
+getMarkerNames <- function(input, output) {
+  fcs <- read.FCS(input, transformation=F)
+
+  ## marker names
+  channels <- colnames(fcs)
+  markers <- as.vector(pData(parameters(fcs))$desc)
+  df <- data.frame(channels, markers)
+  fcs_summary <- capture.output(summary(fcs))
+  fcs_dim <- capture.output(dim(fcs))
+  fcs_markers <- capture.output(df)
 
 
-def get_txt_stats(in_file, out_file):
-    df = pd.read_table(in_file)
-    summary = df.describe().round(1)
-    df1 = summary[1:]
-    x = summary[:1].values.tolist()
-    df1.to_csv(out_file, sep="\t")
-    with open(out_file, "a") as ot:
-        ot.write("\n\n" + str(int(x[0][0])) + " events\n")
-    return
+  sink(output)
+  cat(fcs_dim, sep="\n")
+  cat("\n\n=========================\n")
+  cat("==     FCS SUMMARY     ==\n")
+  cat("=========================\n")
+  cat(fcs_summary, sep="\n")
+  cat("\n\n=========================\n")
+  cat("==    MARKERS IN FCS   ==\n")
+  cat("=========================\n")
+  cat(fcs_markers, sep="\n")
+  sink()
+}
 
+checkFCS <- function(input_file, output_file) {
+  isValid <- F
+  # Check file beginning matches FCS standard
+  tryCatch({
+    isValid <- isFCSfile(input_file)
+  }, error = function(ex) {
+    print (paste("    ! Error in isFCSfile", ex))
+  })
 
-if __name__ == "__main__":
-    parser = ArgumentParser(
-             prog="getTxtStats",
-             description="Prints summary statistics from given file.")
+  if (isValid) {
+    getMarkerNames(input_file, output_file)
+  } else {
+    print (paste(input_file, "does not meet FCS standard"))
+  }
+}
 
-    parser.add_argument(
-            '-i',
-            dest="input_file",
-            required=True,
-            help="File location for the text file.")
-
-    parser.add_argument(
-            '-o',
-            dest="output_file",
-            required=True,
-            help="Name of the output file.")
-
-    args = parser.parse_args()
-
-    get_txt_stats(args.input_file, args.output_file)
+args <- commandArgs(trailingOnly = TRUE)
+checkFCS(args[1], args[2])
