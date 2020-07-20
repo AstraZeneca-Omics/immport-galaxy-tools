26be53bdf960be2936a0b668ad6e867fdcce2227
emily101-gif <52698020+emily101-gif@users.noreply.github.com>
2020-07-20 11:14:56 +0100

    flowtools/flowclr_sample_name/sample_simp.R
    read.table produces a data frame already
    
    Co-authored-by: Pablo Moreno <pcm32@users.noreply.github.com>

--- flowtools/flowclr_sample_name/sample_simp.R
+++ flowtools/flowclr_sample_name/sample_simp.R
@@ -20,7 +20,6 @@ sample_name <- function(input_file, sample_names){
 	flowtext <- list()
 	for(i in input_file){
 		flowtext[i] <- read.table(input_file[i], header = TRUE, sep = "\t")
-		flowtext[i] <- as.data.frame(flowtext[i])	
 	}
 	
 	sample_names <- as.character(sample_names)
@@ -41,5 +40,4 @@ sample_name <- function(input_file, sample_names){
 args <- commandArgs(trailingOnly = TRUE)
 
 sample_name(args[1], args[3])
-
 
